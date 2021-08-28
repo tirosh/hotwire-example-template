150b98d7bb118f0f4472aefd784e27bc8647888b
Sean Doyle <sean.p.doyle24@gmail.com>
2021-08-27 23:43:36 -0400

    [SKIP]: Scaffold Users
    ```sh
    bin/rails generate scaffold User \
      username:citext:index \
      name:citext
    ```
    
    Next, modify the migration by adding `null: false` to both new columns,
    and `unique: true` to the creation of the index.
    
    Then, migrate the database:
    
    ```sh
    bin/rails db:migrate
    ```
    
    Finally, add some fixtures as a [cast of chracters][] often used in
    cryptography examples.
    
    [cast of chracters]: https://en.wikipedia.org/wiki/Alice_and_Bob#Cast_of_characters

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_10_11_133613) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
@@ -51,6 +52,14 @@ ActiveRecord::Schema.define(version: 2021_10_11_133613) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.citext "username"
+    t.citext "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
