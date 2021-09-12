7808f5ff3f7ec0f3a76bf2e066d0009692c0b388
Sean Doyle <sean.p.doyle24@gmail.com>
2021-09-12 18:51:50 -0400

    Our starting point

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_133613) do
+ActiveRecord::Schema.define(version: 2022_02_02_145652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,22 @@ ActiveRecord::Schema.define(version: 2021_10_11_133613) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "applicants", force: :cascade do |t|
+    t.text "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "references", force: :cascade do |t|
+    t.bigint "applicant_id", null: false
+    t.text "name", null: false
+    t.text "email_address", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["applicant_id"], name: "index_references_on_applicant_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "references", "applicants"
 end
---
+++ test/system/applicants_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class ApplicantsTest < ApplicationSystemTestCase
+  test "new presents a form to collect Applicant data" do
+    visit new_applicant_path
+    within :fieldset, "Applicant" do
+      fill_in "Name", with: "Bob"
+    end
+    within :fieldset, "Personal references" do
+      within "li:nth-of-type(1)" do
+        fill_in "Name", with: "Friend"
+        fill_in "Email address", with: "friend@example.com"
+      end
+    end
+    click_on "Create Applicant"
+
+    within :section, "Bob" do
+      assert_text "Friend", count: 1
+      assert_text "friend@example.com", count: 1
+    end
+  end
+
+  test "edit presents a form to edit Applicant data" do
+    alice = applicants :alice
+
+    visit edit_applicant_path(alice)
+    within :fieldset, "Applicant" do
+      fill_in "Name", with: "Alicia"
+    end
+    within :fieldset, "Personal references" do
+      within "li:nth-of-type(1)" do
+        fill_in "Name", with: "Enemy"
+        fill_in "Email address", with: "enemy@example.com"
+      end
+    end
+    click_on "Update Applicant"
+
+    within :section, "Alicia" do
+      assert_text "Enemy", count: 1
+      assert_text "enemy@example.com", count: 1
+    end
+  end
+end
