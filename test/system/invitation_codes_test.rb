ed1b1c755a7065d09ac0ef360f7d3590115adc02
Sean Doyle <sean.p.doyle24@gmail.com>
2022-02-06 23:46:43 -0500

    Our starting point

---
+++ test/system/invitation_codes_test.rb
@@ -0,0 +1,14 @@
+require "application_system_test_case"
+
+class InvitationCodesTest < ApplicationSystemTestCase
+  test "copy code to clipboard" do
+    code = "secret"
+
+    visit invitation_code_path(id: code)
+    click_on "Copy to clipboard"
+    find(:field, "Paste").click
+    send_keys :meta, "v"
+
+    assert_field "Paste", with: code
+  end
+end
